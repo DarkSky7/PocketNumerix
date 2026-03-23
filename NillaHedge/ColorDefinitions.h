0032df1a69c691a8e4d570875a39ed5412520797
Mike Landis <73369410+DarkSky7@users.noreply.github.com>
2026-03-23 02:25:40 -0700

    Mirror from SourceForge PocketNumerix/NillaHedge/ColorDefinitions.h

---
+++ NillaHedge/ColorDefinitions.h
@@ -0,0 +1,5 @@
+// ColorDefinitions.h
+const	COLORREF	bkgndColor = RGB(233,233,188);		// background fill color (manilla 228,228,184)
+const	COLORREF	gridColor  = RGB(191,191,191);		// most grid lines (light Gray)
+const	COLORREF	axisColor  = RGB(159,159,159);		// X-axis (light-mid Gray)
+const	COLORREF	textColor  = RGB( 63, 63, 63);		// labels (dark Gray)
