702cfb7469b1c01bf7502284f7e62d81c49188a8
Mike Landis <73369410+DarkSky7@users.noreply.github.com>
2026-03-23 02:12:30 -0700

    Mirror from SourceForge PocketNumerix/BondManager/utils.h

---
+++ BondManager/utils.h
@@ -0,0 +1,8 @@
+// utils.h
+long			PackOleDate( COleDateTime aDate );
+COleDateTime	UnpackOleDate( long	packedOleDate );
+double			expRand( double lambda );
+//BOOL	DateTimeCtrl_SetRange( HWND hWnd, COleDateTime* pMinTime, COleDateTime* pMaxTime );
+// double		BoxMuller( void );
+//HWND			GlobalGetFocus( void );
+//HWND			PFWGetFocus( void );
