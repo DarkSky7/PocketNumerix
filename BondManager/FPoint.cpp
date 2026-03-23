2545e585a872a0dd4b0b78ebdb958f44f968de5a
Mike Landis <73369410+DarkSky7@users.noreply.github.com>
2026-03-23 02:11:02 -0700

    Mirror from SourceForge PocketNumerix/BondManager/FPoint.cpp

---
+++ BondManager/FPoint.cpp
@@ -0,0 +1,40 @@
+// FPoint.cpp: implementation of the CFPoint class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "StdAfx.h"
+#include "FPoint.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CFPoint::CFPoint()
+{
+}
+
+CFPoint::~CFPoint()
+{
+}
+
+CFPoint::CFPoint( float xin, float yin ) : x(xin), y(yin)
+{
+}
+
+CFPoint::CFPoint( CFPoint& pt1 ) : x(pt1.x), y(pt1.y)
+{
+}
+
+void	CFPoint::operator=( CFPoint& pt1 )
+{
+	x = pt1.x;
+	y = pt1.y;
+
+}
+
