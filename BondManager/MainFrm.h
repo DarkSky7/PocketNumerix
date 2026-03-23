f90a02e6f00c9640fe9cf3504d706a5bc726af00
Mike Landis <73369410+DarkSky7@users.noreply.github.com>
2026-03-23 02:13:30 -0700

    Mirror from SourceForge PocketNumerix/BondManager/MainFrm.h

---
+++ BondManager/MainFrm.h
@@ -0,0 +1,120 @@
+// MainFrm.h : interface of the CMainFrame class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_MAINFRM_H__6CC7283A_1771_4196_A703_25ACAC67830F__INCLUDED_)
+#define AFX_MAINFRM_H__6CC7283A_1771_4196_A703_25ACAC67830F__INCLUDED_
+
+#if _MSC_VER >= 1000
+#pragma once
+#endif // _MSC_VER >= 1000
+
+// #include <Afx.h>			// CDumpContext  ???
+
+// Array for the toolbar buttons
+
+#if (_WIN32_WCE < 201)
+static TBBUTTON g_arCBButtons[] = {
+	{ 0,	ID_FILE_NEW,	TBSTATE_ENABLED, TBSTYLE_BUTTON,	0, 0, 0,  0},
+	{ 1,    ID_FILE_OPEN,	TBSTATE_ENABLED, TBSTYLE_BUTTON,	0, 0, 0,  1},
+	{ 2,	ID_FILE_SAVE,	TBSTATE_ENABLED, TBSTYLE_BUTTON,	0, 0, 0,  2},
+	{ 0,	0,				TBSTATE_ENABLED, TBSTYLE_SEP,		0, 0, 0, -1},
+	{ 3,    ID_EDIT_CUT,	TBSTATE_ENABLED, TBSTYLE_BUTTON,	0, 0, 0,  3},
+	{ 4,	ID_EDIT_COPY,	TBSTATE_ENABLED, TBSTYLE_BUTTON,	0, 0, 0,  4},
+	{ 5,	ID_EDIT_PASTE,	TBSTATE_ENABLED, TBSTYLE_BUTTON,	0, 0, 0,  5}
+};
+#endif
+
+// #if defined(_WIN32_WCE_PSPC) && (_WIN32_WCE >= 212)
+#if ( PPC02_TARGET )
+#define NUM_TOOL_TIPS 8
+#endif
+
+class CMainFrame : public CFrameWnd
+{
+protected: // create from serialization only
+	DECLARE_DYNCREATE(CMainFrame)
+
+// Attributes
+public:
+	CMainFrame();
+
+// Operations
+public:
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CMainFrame)
+	public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+	protected:
+	virtual LRESULT DefWindowProc(UINT message, WPARAM wParam, LPARAM lParam);
+	//}}AFX_VIRTUAL
+
+#if ( ! PPC02_TARGET )			// ( PPC03_TARGET  ||  WM5_TARGET )
+		// this method isn't necessary in MFC3.0, but it's not part of MFC8.0. 
+		// We're approximatng MFC8.0 using the platform, but the real condition
+		// would examine the version of MFC available in the build
+		// Even in MFC3.0, it's an undocumented function.  
+	void	ActivateTopParent( void );
+#endif
+
+// Implementation
+public:
+	virtual ~CMainFrame();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+//	virtual void Dump( CDumpContext& dc ) const;		// won't compile in VS8 given current project settings
+#endif
+
+protected:  // control bar embedded members
+#if ( PPC02_TARGET )
+	CCeCommandBar	m_wndCommandBar;
+#else					// ( PPC03_TARGET  ||  WM5_TARGET )
+	CCommandBar		m_wndCommandBar;
+#endif
+//	CToolBar		m_wndToolBar;
+//	CDialogBar		m_wndDlgBar;
+//	CReBar			m_wndReBar;
+
+// Generated message map functions
+protected:
+	//{{AFX_MSG(CMainFrame)
+	afx_msg int	 OnCreate(LPCREATESTRUCT lpCreateStruct);
+//	afx_msg void OnCreateDocList(DLNHDR* pNotifyStruct, LRESULT* result);
+//	afx_msg void OnDestroyDocList(DLNHDR* pNotifyStruct, LRESULT* result);
+	afx_msg void OnDestroy();
+	afx_msg void OnDefineBond();
+	afx_msg void OnDefineConfirmSaveOptions();
+	afx_msg void OnCreateDisplayPreferences();
+	afx_msg void OnCreatePosition();
+	afx_msg void OnToolsBondAnalyzer();
+	afx_msg void OnToolsPortfolioNavigator();
+	afx_msg void OnToolsViewPositionTranscript();
+	afx_msg void OnFileCloseDatabase();
+	afx_msg void OnFileOpenDatabase();
+	afx_msg void OnUpdateFileCloseDatabase(CCmdUI* pCmdUI);
+	afx_msg void OnUpdateFileOpenDatabase(CCmdUI* pCmdUI);
+	afx_msg void OnUpdateDefineBond(CCmdUI* pCmdUI);
+	afx_msg void OnUpdateCreatePosition(CCmdUI* pCmdUI);
+	afx_msg void OnUpdateToolsBondAnalyzer(CCmdUI* pCmdUI);
+	afx_msg void OnUpdateToolsPortfolioNavigator(CCmdUI* pCmdUI);
+	afx_msg void OnUpdateToolsViewPositionTranscript(CCmdUI* pCmdUI);
+	afx_msg void OnToolsRateSensitivityExplorer();
+	afx_msg void OnUpdateToolsRateSensitivityExplorer(CCmdUI* pCmdUI);
+//	afx_msg BOOL OnIconNotify();										// ON_COMMAND - never gets called
+	afx_msg LRESULT OnIconNotify( WPARAM, LPARAM );						// ON_MESSAGE format
+	afx_msg LRESULT OnBondManagerAlreadyRunning( WPARAM, LPARAM );
+	//}}AFX_MSG
+
+	DECLARE_MESSAGE_MAP()
+
+	LPTSTR MakeString(UINT stringID);
+#if ( PPC02_TARGET )
+	LPTSTR m_ToolTipsTable[NUM_TOOL_TIPS];
+#endif
+};
+/////////////////////////////////////////////////////////////////////////////
+//{{AFX_INSERT_LOCATION}}
+// Microsoft eMbedded Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_MAINFRM_H__6CC7283A_1771_4196_A703_25ACAC67830F__INCLUDED_)
