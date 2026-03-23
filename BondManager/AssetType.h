01ab019f3595f41aa26b6b5d11a0d4782ee1e4d1
Mike Landis <73369410+DarkSky7@users.noreply.github.com>
2026-03-23 02:12:24 -0700

    Mirror from SourceForge PocketNumerix/BondManager/AssetType.h

---
+++ BondManager/AssetType.h
@@ -0,0 +1,19 @@
+#if !defined(AFX_ASSETTYPE_H__BF9849X7R3C_AE1F_4C52_95B7_A4A03AA8C4F2__INCLUDED_)
+#define      AFX_ASSETTYPE_H__BF9849X7R3C_AE1F_4C52_95B7_A4A03AA8C4F2__INCLUDED_
+
+#if _MSC_VER >= 1000
+#pragma once
+#endif // _MSC_VER >= 1000
+
+#ifndef __AFXWIN_H__
+	#error include 'StdAfx.h' before including this file for PCH
+#endif
+
+enum AssetType
+{	UnknownAssetType		=  0,
+	Bond					=  1,
+	Option					=  2,
+	Stock					=  3
+};
+
+#endif	// !defined(AFX_ASSETTYPE_H__BF9849X7R3C_AE1F_4C52_95B7_A4A03AA8C4F2__INCLUDED_)
