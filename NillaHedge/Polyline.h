50b3b6832ed664c0f1f3061d4caadfdb4aaaf1ea
Mike Landis <73369410+DarkSky7@users.noreply.github.com>
2026-03-23 02:22:58 -0700

    Mirror from SourceForge PocketNumerix/NillaHedge/Polyline.h

---
+++ NillaHedge/Polyline.h
@@ -0,0 +1,33 @@
+// Polyline.h: interface for the CPolyline class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_POLYLINE_H__494DE183_D29D_48B5_A164_2B785584A75B__INCLUDED_)
+#define AFX_POLYLINE_H__494DE183_D29D_48B5_A164_2B785584A75B__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class	CLineSeg;
+class	CFPoint;
+
+class CPolyline
+{
+public:
+	CPolyline();
+	virtual			~CPolyline();
+	void			InsertPoint( CFPoint& anFPoint );	// if it's not already there
+	void			BoostPoint( CFPoint& cfp );			// adds cfp.y to the poly point matching cfp.x
+	void			BoostSegment( CLineSeg& seg1, CFPoint& skipPt );
+		// Accessors
+	short			GetPointCount( void )		{	return pts.GetSize();	}
+	CLineSeg*		GetSegment( short ii );		// ii is 0-based
+	void			Dump( void );
+
+protected:
+	CObArray		pts;		// at x=0; at each of 3 strikes; at a big number
+};
+#endif // !defined(AFX_POLYLINE_H__494DE183_D29D_48B5_A164_2B785584A75B__INCLUDED_)
+
+//	void			BoostPoint( CFPoint& cfp );
