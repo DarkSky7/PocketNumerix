8b69ec01127cdb2f85e82d327aaf7fbd01150280
Mike Landis <73369410+DarkSky7@users.noreply.github.com>
2026-03-23 01:45:27 -0700

    Mirror from SourceForge PocketNumerix/StrategyExplorer/utils.h

---
+++ StrategyExplorer/utils.h
@@ -0,0 +1,9 @@
+// utils.h
+
+long			PackOleDate( COleDateTime aDate );
+COleDateTime	UnpackOleDate( long	packedOleDate );
+double			expRand( double lambda );
+double			Discretize( double	dd );
+// double		BoxMuller( void );
+//HWND			GlobalGetFocus( void );
+//HWND			PFWGetFocus( void );
