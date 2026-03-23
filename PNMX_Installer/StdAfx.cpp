a254f7a2b26b1854af3f2140c46ce988fb54da77
Mike Landis <73369410+DarkSky7@users.noreply.github.com>
2026-03-23 00:42:34 -0700

    Mirror from SourceForge PocketNumerix/PNMX_Installer/StdAfx.cpp

---
+++ PNMX_Installer/StdAfx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+// PNMX_Installer.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "StdAfx.h"
+
+
