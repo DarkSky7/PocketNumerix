110d9ef96f94f443724acf635a0bd2c20586cd4b
Mike Landis <73369410+DarkSky7@users.noreply.github.com>
2026-03-23 00:43:48 -0700

    Mirror from SourceForge PocketNumerix/WebEncodeDecode/CipherKey.php

---
+++ WebEncodeDecode/CipherKey.php
@@ -0,0 +1,6 @@
+<?php
+	$nCipherKeys = 8;
+	$CipherKey = pack( "VVVVVVVV",
+					0x83DE9C41, 0x28AED2A6, 0x09Cf4f3C, 0x731FE385,
+					0x2B7E1516, 0xAB710588, 0x9B7E1839, 0x36C0F31D  );									
+?>
