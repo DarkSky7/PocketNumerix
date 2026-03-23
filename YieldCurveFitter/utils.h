c7340bff3e97163b462c4898197c0e32a4eb4ea7
Mike Landis <73369410+DarkSky7@users.noreply.github.com>
2026-03-23 01:57:31 -0700

    Mirror from SourceForge PocketNumerix/YieldCurveFitter/utils.h

---
+++ YieldCurveFitter/utils.h
@@ -0,0 +1,6 @@
+// utils.h
+char*			GetThinString( CString cs );
+long			PackOleDate( COleDateTime aDate );
+COleDateTime	UnpackOleDate( long	packedOleDate );
+double			expRand( double lambda );
+// double		BoxMuller( void );
