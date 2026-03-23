417ae1ebdc51a316a2ea579e939c9cf5c2bf568d
Mike Landis <73369410+DarkSky7@users.noreply.github.com>
2026-03-23 02:47:25 -0700

    Mirror from SourceForge PocketNumerix/OptionMiner/ProfileParser.cpp

---
+++ OptionMiner/ProfileParser.cpp
@@ -0,0 +1,22 @@
+// ProfileParser.cpp
+
+#include "StdAfx.h"
+#include "ParseSupport.h"
+#include <string.h>								// getline()
+
+
+//--------------------------------------------------------------------------------------
+short	ParseProfile( void )
+{		// open OptionMiner.prf
+	FILE* fp = fopen( "OptionsMiner.prf", "r" );
+	if ( !fp )
+	{	return	-1;
+	}
+	char	buf[256];
+	char*	bb = buf;
+	while ( )
+	{	
+	}
+	return	0;
+}			//
+//--------------------------------------------------------------------------------------
